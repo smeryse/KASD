8849272c456057e989b4002fd657b7bb55bc9f28
Smeryse <littlebigonelive@gmail.com>
2025-09-16 14:46:11 +0300

    vault backup: 2025-09-16 14:46:11

---
+++ Практики/ЛР3/ЛР3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ЛР3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Name: {0}", drive.Name);
+                Console.WriteLine($"Type: {0}", drive.DriveType);
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Drive total: {0}", drive.TotalSize);
+
+                }
+            }
+        }
+    }
+}
