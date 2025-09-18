a8494c19b1b6e5d25ebcc1ff4a0f1386315bd992
Smeryse <littlebigonelive@gmail.com>
2025-09-18 16:16:57 +0300

    vault backup: 2025-09-18 16:16:57

--- Практики/ЛР2/ЛР2/State.cs
+++ Практики/ЛР2/ЛР2/State.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +14,31 @@ namespace ЛР2
         public uint Population { get; set; }
         public uint Area { get; set; }
         public static State operator +(State s1, State s2)
-            {
-            }
+        {
+            string name = s1.Name;
+            uint population = s1.Population + s2.Population;
+            uint area = s1.Area + s2.Area;
+            return new State { Name = name, Population = population, Area = area };
+        }
+        public static bool operator >(State s1, State s2)
+        {
+            if (s1.Area > s2.Area) { return true; }
+            else { return false; }
+        }
+        public static bool operator <(State s1, State s2)
+        {
+            if (s1.Area < s2.Area) { return true; }
+            else { return false; }
+        }
+        public static bool operator ==(State s1, State s2)
+        {
+            if (s1.Area == s2.Area) { return true; }
+            else { return false; }
+        }
+        public static bool operator !=(State s1, State s2)
+        {
+            if (s1.Area != s2.Area) { return true; }
+            else { return false; }
+        }
     }
 }
