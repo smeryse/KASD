328e16dd2a884cf43269fe041da392feef23118e
Smeryse <littlebigonelive@gmail.com>
2025-09-18 15:53:14 +0300

    vault backup: 2025-09-18 15:53:13

---
+++ Практики/ЛР2/ЛР2/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР2
+{
+    class Employee : Person
+    {
+        public string Company { get; set; }
+        public Employee(string lName, string fName, string comp)
+            : base(fName, lName)
+        {
+            Company = comp;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine(FirstName + " " + LastName + " работает в компании " + Company); 
+        }
+    }
+}
