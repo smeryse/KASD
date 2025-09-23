bbe11bc62a2ca8ab50be414fa2a17ceaa4c9c7a1
Smeryse <littlebigonelive@gmail.com>
2025-09-23 14:39:37 +0300

    vault backup: 2025-09-23 14:39:36

--- Практики/ЛР2/ЛР2/Kingdom.cs
+++ Практики/ЛР2/ЛР2/Kingdom.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace ЛР2
 {
-    class Kingdom
+    class Kingdom : ConstitutionalMonarchy
     {
+        private string _capital;
+        public string Capital
+        {
+            get => _capital;
+            set => _capital = !string.IsNullOrWhiteSpace(value) ? value
+                : throw new ArgumentException("Название столицы не может быть пустым");
+        }
+
         // Дополнительные свойства: Capital, Regions (массив)
-        // Методы: RoyalTour(), DisplayRegions()
         // Переопределить Coronation() и ToString()
+        public Kingdom(string name, uint population, uint area, string monarch, string dynasty, string capital) : base(name, population, area, monarch, dynasty)
+        {
+            Capital = capital;
+        }
     }
 }
