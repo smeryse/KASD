eeb6416f5db4887bdc45d71b16ca338c78081278
smeryse <luxiut@yandex.ru>
2025-10-02 16:05:41 +0300

    vault backup: 2025-10-02 16:05:41

--- Практики/ЛР3/Models/Group.cs
+++ Практики/ЛР3/Models/Group.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-
 class Group
 {
     public int GroupId { get; set; }
     public string GroupName { get; set; }
     public List<Student> Students { get; set; } = new List<Student>();
 
+    public Group(string groupName)
+    {
+        GroupName = groupName;
+    }
+
+    public void AddStudent(Student student) => Students.Add(student);
     public void Print()
     {
-
+        Console.WriteLine($"Группа: {GroupName}");
+        foreach (var student in Students)
+            student.Print();
     }
 }
