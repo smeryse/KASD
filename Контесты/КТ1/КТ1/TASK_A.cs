8ec1e422581c78fcc4fc1224db0a2f2b350a17f7
smeryse <luxiut@yandex.ru>
2025-10-21 13:28:05 +0300

    vault backup: 2025-10-21 13:28:05

--- Контесты/КТ1/КТ1/TASK_A.cs
+++ Контесты/КТ1/КТ1/TASK_A.cs
@@ -1,33 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-    
 class MergeSorter
 {
     public int[] Array { get; private set; }
 
     public MergeSorter(int[] arr) => Array = arr;
+
+    // Публичный метод для сортировки
+    public void Sort()
+    {
+        if (Array == null || Array.Length <= 1) return;
+        merge_sort(0, Array.Length - 1);
+    }
+
+    // Рекурсивная сортировка
     private void merge_sort(int left, int right)
     {
         if (left >= right) return;
+
         int mid = (left + right) / 2;
 
-        int amount_left = mid - left + 1;
-        int amount_right = right - mid;
+        merge_sort(left, mid);        // сортируем левую половину
+        merge_sort(mid + 1, right);   // сортируем правую половину
 
-        int[] left_half = new int[amount_left];
-        int[] right_half = new int[amount_right];
-
-        Array.CopyTo(
-            Array, left, left_half, amount_left);
-        Array.CopyTo(left_half, mid);
+        merge(left, mid, right);      // сливаем отсортированные части
     }
-    void merge()
-    {
 
+    // Слияние двух отсортированных подмассивов
+    private void merge(int left, int mid, int right)
+    {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        Array.Copy(Array, left, L, 0, n1);
+        Array.Copy(Array, mid + 1, R, 0, n2);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+                Array[k++] = L[i++];
+            else
+                Array[k++] = R[j++];
+        }
+
+        while (i < n1) Array[k++] = L[i++];
+        while (j < n2) Array[k++] = R[j++];
     }
 }
