e1629cb796a0d1217e82d2829e97ffb5f4b08c0b
smeryse <luxiut@yandex.ru>
2025-10-21 16:01:37 +0300

    vault backup: 2025-10-21 16:01:37

--- Контесты/КТ1/КТ2/CountingSort.cs
+++ Контесты/КТ1/КТ2/CountingSort.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using System.Linq;
+﻿//using System;
+//using System.Linq;
 
-class Program
-{
-    static void CountingSort(int[] arr)
-    {
-        if (arr.Length == 0) return;
+//class Program
+//{
+//    static void CountingSort(int[] arr)
+//    {
+//        if (arr.Length == 0) return;
 
-        int max = arr[0];
-        int min = arr[0];
+//        int max = arr[0];
+//        int min = arr[0];
 
-        foreach (int num in arr)
-        {
-            if (num > max) max = num;
-            if (num < min) min = num;
-        }
+//        foreach (int num in arr)
+//        {
+//            if (num > max) max = num;
+//            if (num < min) min = num;
+//        }
 
-        int range = max - min + 1;
-        int[] count = new int[range];
+//        int range = max - min + 1;
+//        int[] count = new int[range];
 
-        foreach (int num in arr)
-        {
-            count[num - min]++;
-        }
-        int index = 0;
-        for (int i = 0; i < range; i++)
-        {
-            while (count[i] > 0)
-            {
-                arr[index++] = i + min;
-                count[i]--;
-            }
-        }
-    }
-    static void Main(string[] args)
-    {
-        Console.ReadLine();
-        int[] arr = Console.ReadLine()
-                   .Split(' ')
-                   .Select(int.Parse)
-                   .ToArray();
-        CountingSort(arr);
-        Console.WriteLine(string.Join(" ", arr));
-    }
-}
+//        foreach (int num in arr)
+//        {
+//            count[num - min]++;
+//        }
+//        int index = 0;
+//        for (int i = 0; i < range; i++)
+//        {
+//            while (count[i] > 0)
+//            {
+//                arr[index++] = i + min;
+//                count[i]--;
+//            }
+//        }
+//    }
+//    static void Main(string[] args)
+//    {
+//        Console.ReadLine();
+//        int[] arr = Console.ReadLine()
+//                   .Split(' ')
+//                   .Select(int.Parse)
+//                   .ToArray();
+//        CountingSort(arr);
+//        Console.WriteLine(string.Join(" ", arr));
+//    }
+//}
