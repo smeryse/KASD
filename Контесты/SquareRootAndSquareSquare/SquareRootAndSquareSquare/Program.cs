987bbb3da192d396b2f4d1db8b86d873d2943be8
smeryse <luxiut@yandex.ru>
2025-10-23 22:42:21 +0300

    vault backup: 2025-10-23 22:42:21

---
+++ Контесты/SquareRootAndSquareSquare/SquareRootAndSquareSquare/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+
+        double C = 10; // пример
+        Application.Run(new BinaryTreeForm(C));
+    }
+}
+
+class Interval
+{
+    public double Left, Right, Mid;
+    public int Depth;
+    public Interval(double l, double r, int d)
+    {
+        Left = l; Right = r; Depth = d;
+        Mid = (l + r) / 2.0;
+    }
+}
+
+class BinaryTreeForm : Form
+{
+    double C;
+    double eps = 1e-7;
+    List<Interval> intervals = new List<Interval>();
+
+    public BinaryTreeForm(double C)
+    {
+        this.C = C;
+        this.Width = 800;
+        this.Height = 600;
+        this.Text = "Бинарный поиск как дерево";
+
+        this.DoubleBuffered = true;
+
+        InitTree();
+    }
+
+    void InitTree()
+    {
+        double left = 0;
+        double right = Math.Max(1, C);
+
+        Queue<Interval> queue = new Queue<Interval>();
+        queue.Enqueue(new Interval(left, right, 0));
+
+        while (queue.Count > 0)
+        {
+            var iv = queue.Dequeue();
+            intervals.Add(iv);
+
+            if (iv.Right - iv.Left < eps)
+                continue;
+
+            double f = iv.Mid * iv.Mid + Math.Sqrt(iv.Mid);
+            if (f < C)
+                queue.Enqueue(new Interval(iv.Mid, iv.Right, iv.Depth + 1));
+            else
+                queue.Enqueue(new Interval(iv.Left, iv.Mid, iv.Depth + 1));
+        }
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+        Graphics g = e.Graphics;
+        g.Clear(Color.White);
+
+        int marginX = 50;
+        int marginY = 50;
+        int levelHeight = 40;
+
+        int width = this.ClientSize.Width - 2 * marginX;
+
+        DrawNode(g, intervals[0], marginX, marginY, width);
+    }
+
+    void DrawNode(Graphics g, Interval iv, int x, int y, int width)
+    {
+        int nodeX = x + (int)((iv.Mid - iv.Left) / (iv.Right - iv.Left) * width);
+        int nodeY = y;
+
+        // рисуем узел
+        g.FillEllipse(Brushes.Green, nodeX - 5, nodeY - 5, 10, 10);
+        g.DrawString(iv.Mid.ToString("F4"), new Font("Arial", 8), Brushes.Black, nodeX + 5, nodeY - 10);
+
+        // ищем детей
+        var children = intervals.FindAll(ch => ch.Depth == iv.Depth + 1 &&
+            ((ch.Left == iv.Left && ch.Right == iv.Mid) || (ch.Left == iv.Mid && ch.Right == iv.Right)));
+
+        int childY = y + 40;
+        int childWidth = width / 2;
+
+        foreach (var child in children)
+        {
+            int childX = x + (int)((child.Mid - child.Left) / (child.Right - child.Left) * childWidth);
+            // рисуем линию
+            g.DrawLine(Pens.Black, nodeX, nodeY, childX, childY);
+
+            DrawNode(g, child, x, childY, childWidth);
+        }
+    }
+}
---
+++ Контесты/КТ1/КТ1/K-best [WIP].cs
@@ -0,0 +1,53 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        var args = Console.ReadLine().Split(' ');
+//        int n = int.Parse(args[0]);
+//        int k = int.Parse(args[1]);
+
+//        var gems = new List<(int value, int weight, int index)>();
+
+//        for (int i = 0; i < n; i++)
+//        {
+//            var parts = Console.ReadLine().Split(' ');
+//            int v = int.Parse(parts[0]);
+//            int w = int.Parse(parts[1]);
+//            gems.Add((v, w, i + 1));
+//        }
+
+//        double left = 0;
+//        double right = gems.Max(g => (double)g.value / g.weight);
+//        double eps = 1e-7;
+
+//        List<int> answerIndices = null;
+
+//        while (right - left > eps)
+//        {
+//            double mid = (left + right) / 2;
+
+//            var b = gems.Select(g => (g.value - mid * g.weight, g.index)).ToList();
+
+//            b.Sort((a, c) => c.Item1.CompareTo(a.Item1));
+//            double sum = 0;
+//            for (int i = 0; i < k; i++) sum += b[i].Item1;
+
+//            if (sum >= 0)
+//            {
+//                left = mid;
+//                answerIndices = b.Take(k).Select(x => x.Item2).ToList();
+//            }
+//            else
+//            {
+//                right = mid;
+//            }
+//        }
+
+//        foreach (var idx in answerIndices)
+//            Console.WriteLine(idx);
+//    }
+//}
