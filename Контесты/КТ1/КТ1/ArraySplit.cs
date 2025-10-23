a0753053c3f7df4ecd2f8312f570f54d791cbfc5
smeryse <luxiut@yandex.ru>
2025-10-23 23:51:42 +0300

    vault backup: 2025-10-23 23:51:42

---
+++ Контесты/КТ1/КТ1/ArraySplit.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        string[] args = Console.ReadLine().Split(' ');
+//        int n = int.Parse(args[0]);    
+//        int k = int.Parse(args[1]);
+
+//        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+//    }
+//}
