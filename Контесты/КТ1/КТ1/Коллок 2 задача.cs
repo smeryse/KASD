a89a8a3d1cb96861c01a2ac31a70fe88d5efbef4
smeryse <luxiut@yandex.ru>
2025-11-01 13:09:25 +0300

    vault backup: 2025-11-01 13:09:25

---
+++ Контесты/КТ1/КТ1/Коллок 2 задача.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        int[] arr = { 1, 1, 2, 2, 3 };
+//        Dictionary<int, int> counter = new Dictionary<int, int>();
+
+//        for (int i = 0; i < arr.Length; i++)
+//        {
+//            counter[arr[i]] = 0;
+//        }
+
+//        for (int i = 0; i < arr.Length; i++)
+//        {
+//            counter[arr[i]] += 1;
+//        }
+
+//        foreach (int i in counter.Keys)
+//        {
+//            if (counter[i] == 1) Console.WriteLine(i);
+//        }
+//    }
+
+//}
