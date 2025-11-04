aa4f623d872d8ae117ebae2b787d7a5ed9c2fd5e
smeryse <luxiut@yandex.ru>
2025-11-04 14:57:21 +0300

    vault backup: 2025-11-04 14:57:21

--- Контесты/КТ1/КТ1/Коллок 2 задача.cs
+++ Контесты/КТ1/КТ1/Коллок 2 задача.cs
@@ -11,13 +11,13 @@ class Program
 
     static void RunTests()
     {
-        //Test(new int[] { 2, 4, 7, 11, 15, 21, 30 }, 2, 55);
-        //Test(new int[] { 2, 4, 7, 11, 15, 21, 30 }, 3, 36);
-        //Test(new int[] { 1, 2, 3, 4, 5 }, 2, 9);
+        Test(new int[] { 2, 4, 7, 11, 15, 21, 30 }, 2, 55);
+        Test(new int[] { 2, 4, 7, 11, 15, 21, 30 }, 3, 36);
+        Test(new int[] { 1, 2, 3, 4, 5 }, 2, 9);
         Test(new int[] { 5, 5, 5, 5, 5 }, 3, 10);
-        //Test(new int[] { 10, 20, 30 }, 1, 60);
-        //Test(new int[] { 10, 20, 30 }, 3, 30);
-        //Test(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 4, 15);
+        Test(new int[] { 10, 20, 30 }, 1, 60);
+        Test(new int[] { 10, 20, 30 }, 3, 30);
+        Test(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 4, 15);
     }
 
     static void Test(int[] arr, int k, int expected)
@@ -32,7 +32,7 @@ class Program
 
     static int BinarySearch(int k, int left, int right, int[] arr)
     {
-        if (left == right) return left;
+        if (left >= right) return left;
 
         int mid = (left + right) / 2;
         if (CanSplit(k, mid, arr)) return BinarySearch(k, left, mid - 1, arr);
