5bb9488aebd1a997ad75502e3bed6074b930e91b
smeryse <luxiut@yandex.ru>
2025-11-04 13:01:56 +0300

    vault backup: 2025-11-04 13:01:56

--- Контесты/КТ1/КТ1/Коллок 2 задача.cs
+++ Контесты/КТ1/КТ1/Коллок 2 задача.cs
@@ -1,13 +1,18 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
     static void Main()
     {
+        int[] arr = { 2, 4, 7, 11, 15, 21, 30 };
+        int num = 11;
 
+        Console.WriteLine(BinarySearch());
     }
-    static int BinarySearch(int i, int left, int right, int[] arr)
+    static int BinarySearch(int num, int left, int right, int[] arr)
     {
         int mid = (left + right) / 2;
-        if (arr[i] > mid) return BinarySearch(i, mid, right);
-        else return BinarySearch(i, left, mid);
+        if (num > arr[mid]) return BinarySearch(num, mid, right, arr);
+        else return BinarySearch(num, left, mid, arr);
     }
 }
