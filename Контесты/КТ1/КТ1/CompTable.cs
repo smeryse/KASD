fa1d4d2ed5a5892b07556f7f6b35d6d2ab84b560
smeryse <luxiut@yandex.ru>
2025-11-04 17:22:49 +0300

    vault backup: 2025-11-04 17:22:49

--- Контесты/КТ1/КТ1/CompTable.cs
+++ Контесты/КТ1/КТ1/CompTable.cs
@@ -7,25 +7,25 @@ class Program
     static int[] arr;
     static void Main(string[] args)
     {
-        n = 9;
+        n = 3;
         k = 4;
 
-        Console.WriteLine(CountBefore(35));
+        Console.WriteLine(BinarySearch(1, n*n));
     }
 
     static int BinarySearch(int left, int right)
     {
         if (left == right) return left;
         int mid = (left + right) / 2;
-        if (CountBefore(mid) > k) return BinarySearch(left, mid);
-        else return BinarySearch(mid, right);
+        if (CountBefore(mid) > k) return BinarySearch(left, mid - 1);
+        else return BinarySearch(mid + 1, right);
     }
     static int CountBefore(int num)
     {
         int result = 0;
         for (int i = 1; i <= n; i++)
         {
-            result += (num - 1) / i;
+            result += Math.Min(n, (num - 1) / i);
         }
         return result - 1;
     }
