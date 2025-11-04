ff455a7b34ccd385abdbc2cc8ae9c7982554ded7
smeryse <luxiut@yandex.ru>
2025-11-04 13:00:44 +0300

    vault backup: 2025-11-04 13:00:44

--- Контесты/КТ1/КТ1/Коллок 2 задача.cs
+++ Контесты/КТ1/КТ1/Коллок 2 задача.cs
@@ -1,26 +1,13 @@
-﻿using System;
-class Program
+﻿class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        int[] values = { 60, 100, 120 };
-        int[] weight = { 10, 20, 30};
-        int W = 50;
-        int n = values.Length;
 
-        Console.WriteLine(knapSack(W, n, weight, values));
     }
-    static int knapSack(int W, int n, int[] weight, int[] values)
+    static int BinarySearch(int i, int left, int right, int[] arr)
     {
-        if (n == 0 || W == 0) return 0;
-
-        if (weight[n - 1] > W) return knapSack(W, n - 1, weight, values);
-        else
-        {
-            return Math.Max(
-                knapSack(W, n - 1, weight, values), 
-                values[n - 1] + knapSack(W - weight[n - 1], n - 1, weight, values)
-                );
-        }
+        int mid = (left + right) / 2;
+        if (arr[i] > mid) return BinarySearch(i, mid, right);
+        else return BinarySearch(i, left, mid);
     }
 }
