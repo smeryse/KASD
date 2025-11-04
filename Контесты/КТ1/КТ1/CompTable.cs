7da6d59a1487d5a18cf9cb851140f6ae0e5982c1
smeryse <luxiut@yandex.ru>
2025-11-04 15:50:58 +0300

    vault backup: 2025-11-04 15:50:58

---
+++ Контесты/КТ1/КТ1/CompTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            for (int j = 1; j <= 3; j++)
+            {
+                Console.WriteLine(i*j);
+            }
+        }
+    }
+}
