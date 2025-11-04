6cdc7e3c2015498f005c051d098226e4cb3626dd
smeryse <luxiut@yandex.ru>
2025-11-04 17:55:54 +0300

    vault backup: 2025-11-04 17:55:54

--- Контесты/КТ1/КТ1/KSumm.cs
+++ Контесты/КТ1/КТ1/KSumm.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
-namespace КТ1
+class Program
 {
-    internal class KSumm
+    static void Main()
     {
+        int n = 5;
+        int k = 10;
+        int[] a = { 4, 2, 6, 4, 8 };
+        int[] b = { 7, 3, 1, 9, 5 };
+
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                result.Add(a[i] + b[j]);
+            }
+        }
+        
+        result.Sort();
+        Console.WriteLine(result[k]);
     }
 }
