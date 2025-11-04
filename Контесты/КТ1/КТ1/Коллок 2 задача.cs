f1152aa9dacca9d7e3fd66c42c8a9c426fff4a59
smeryse <luxiut@yandex.ru>
2025-11-04 14:03:00 +0300

    vault backup: 2025-11-04 14:03:00

--- Контесты/КТ1/КТ1/Коллок 2 задача.cs
+++ Контесты/КТ1/КТ1/Коллок 2 задача.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Security.AccessControl;
 
 class Program
 {
@@ -6,13 +8,36 @@ class Program
     {
         int[] arr = { 2, 4, 7, 11, 15, 21, 30 };
         int num = 11;
+        BinarySearch(num, arr.Max(), arr.Sum(), arr);
 
-        Console.WriteLine(BinarySearch());
+        Console.WriteLine(BinarySearch(num, 0, arr.Length - 1, arr));
     }
+
     static int BinarySearch(int num, int left, int right, int[] arr)
     {
         int mid = (left + right) / 2;
-        if (num > arr[mid]) return BinarySearch(num, mid, right, arr);
-        else return BinarySearch(num, left, mid, arr);
+        if (num > mid)
+        {
+
+        }
+    }
+
+    static bool CanSplit(int k, int maxSum, int[] arr)
+    {
+        int temp = 0;
+        int count = 0;
+        foreach (int i in arr)
+        {
+            if (temp >= maxSum)
+            {
+                temp = 0;
+                count++;
+            }
+            else
+            {
+                temp += i;
+            }
+        }
+        return count == k;
     }
 }
